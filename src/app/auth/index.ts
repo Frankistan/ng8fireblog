0ee750870157fb6d3189519cfaef92f9c3b7179c
Frankistan <fffernandez84@gmail.com>
2019-08-14 21:35:16 +0200

    Commit #5

--- src/app/auth/index.ts
+++ src/app/auth/index.ts
@@ -1,4 +1,4 @@
+export * from './auth.component';
 export * from './login/login.component';
 export * from './reset-password/reset-password.component';
 export * from './signup/signup.component';
-export * from './auth.component';
